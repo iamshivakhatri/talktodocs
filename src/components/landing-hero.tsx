67f7fc539286a2662b11db078afaeb60a53be5f8
Shiva Khatri <shiva.khatri0001@gmail.com>
2024-09-20 23:07:15 -0400

    Working on landing page ui.

--- src/components/landing-hero.tsx
+++ src/components/landing-hero.tsx
@@ -34,53 +34,6 @@ const LandingHero = () => {
 
             </div>
 
-            <div>
-
-               <div className="w-4/5 md:w-3/4 m-auto h-32 sm:h-48 md:h-64 border-4 border-dashed border-gray-300 rounded-lg flex items-center justify-center text-center p-4 bg-gray-100">
-                <input
-                    type="file"
-                    id="file-upload"
-                    className="hidden"
-                    accept="audio/*,video/*,application/pdf"
-                    multiple
-                />
-                <label
-                    htmlFor="file-upload"
-                    className="w-full h-full flex flex-col items-center justify-center cursor-pointer"
-                >
-                    <svg
-                    className="w-12 h-12 text-gray-400 mb-3"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                    >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M7 16l-4-4m0 0l4-4m-4 4h18M5 12h7m0 0l-4 4m4-4l4-4"
-                    ></path>
-                    </svg>
-                    <p className="text-gray-500">
-                    Drag & drop your audio, video, or PDF files here, or{" "}
-                    <span className="text-blue-500 underline">browse</span> to upload
-                    </p>
-                </label>
-                </div>
-
-                <span className="mt-2 mb-2">
-                    OR
-                </span>    
-                <div>
-                    <input
-                    type="text"
-                    id="text-upload"
-                    placeholder="Paste a YouTube link"
-                    className="w-4/5 md:w-3/4 m-auto text-gray-500 h-12 border-4 border-dashed border-gray-300 rounded-lg text-center p-4 bg-gray-100 "
-                    />
-                </div>
-            </div>
 
             
             <div>

026677c8ae202fbe5780bee6c46e7cca1c23e0f5
Shiva Khatri <shiva.khatri0001@gmail.com>
2024-10-20 10:52:55 -0400

    Fixed record and play

--- src/components/record-and-play-audio.tsx
+++ src/components/record-and-play-audio.tsx
@@ -186,7 +186,8 @@ const RecordAndPlayAudio: React.FC<RecordAndPlayAudioProps> = ({isPro, numberOfM
     };
 
     if (!browserSupportsSpeechRecognition) {
-        return <span>Browser doesn't support speech recognition.</span>;
+        return <span>Browser doesn&apos;t support speech recognition.</span>;
+
     }
 
     return (

d5be969e684dd70449964b48043a3f705dbefa6b
Shiva Khatri <shiva.khatri0001@gmail.com>
2024-10-21 23:39:37 -0400

    Fixed the next config file.

--- next.config.mjs
+++ next.config.mjs
@@ -1,42 +1,37 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    webpack: (config, { isServer, dev }) => {
-      // Fixes npm packages that depend on `fs` module
-      if (!isServer) {
-        config.resolve.fallback = {
-          ...config.resolve.fallback,
-          fs: false,
-          net: false,
-          tls: false,
-        };
-      }
-  
-      // Add a rule to handle the `regenerator-runtime` module
-      config.module.rules.push({
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', { targets: "defaults" }]
-            ],
-            plugins: ['@babel/plugin-transform-runtime']
-          }
-        }
-      });
-  
-      return config;
-    },
-  };
-  
-//   module.exports = nextConfig;
+  reactStrictMode: false,
+  webpack: (config, { isServer, dev }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
 
-  export default nextConfig;
+    // Add a rule to handle the `regenerator-runtime` module
+    config.module.rules.push({
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [['@babel/preset-env', { targets: "defaults" }]],
+          plugins: ['@babel/plugin-transform-runtime'],
+        },
+      },
+    });
+
+    return config;
+  },
+  images: {
+    domains: ['i.imgur.com'],
+  },
+};
+
+// Correct syntax for ES module
+export default nextConfig;
